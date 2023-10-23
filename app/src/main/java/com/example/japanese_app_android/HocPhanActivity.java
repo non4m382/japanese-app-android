ede5741d33c1a234ab224e9f3d8d5ae6f8d83ecc
minhnhat29 <minhgipsy2992002@gmail.com>
2023-10-23 15:45:30 +0700

    hocphan screen

---
+++ app/src/main/java/com/example/japanese_app_android/HocPhanActivity.java
@@ -0,0 +1,14 @@
+package com.example.japanese_app_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HocPhanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hoc_phan);
+    }
+}
