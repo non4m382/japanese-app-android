9bf3868a3232ce4f715a0e19664643bb1059abe2
ThanhDq8902 <quangthanh08092002@gmail.com>
2023-11-04 17:30:28 +0700

    create FE lesson

---
+++ app/src/main/java/com/example/japanese_app_android/adapter/KanjiLessonAdapter.java
@@ -0,0 +1,62 @@
+package com.example.japanese_app_android.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.japanese_app_android.R;
+import com.example.japanese_app_android.model.KanjiLesson;
+
+import java.util.List;
+
+public class KanjiLessonAdapter extends RecyclerView.Adapter<KanjiLessonAdapter.ViewHolder> {
+    private List<KanjiLesson> lessonList;
+    private Context context;
+
+    public KanjiLessonAdapter(Context context, List<KanjiLesson> lessonList) {
+        this.context = context;
+        this.lessonList = lessonList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Inflate the layout for the lesson
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.kanji_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        if (lessonList != null && position < lessonList.size()) {
+            KanjiLesson lesson = lessonList.get(position);
+            if (lesson != null) {
+                // Set the lesson name and content
+                holder.tvName.setText(lesson.getName());
+                holder.tvContent.setText(lesson.getContent());
+            }
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        // Return the total number of lessons
+        return lessonList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView tvName;
+        public TextView tvContent;
+
+        public ViewHolder(View view) {
+            super(view);
+            // Initialize the TextViews
+            tvName = view.findViewById(R.id.idNameKanJi);
+            tvContent = view.findViewById(R.id.idContentKanJi);
+        }
+    }
+}
